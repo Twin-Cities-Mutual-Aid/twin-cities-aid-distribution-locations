6ebaf8dd70dec8c38870f8cd84e1d164e3b0de2d
Andy Dayton <amdayton@gmail.com>
2020-06-04 17:20:15 -0500

    camelCase -> snakeCase for language tranlsation ids

--- filter.js
+++ filter.js
@@ -60,12 +60,12 @@ class Filter {
   renderControls() {
     
     const options = this.sortOptions.map(o => {
-      return `<option data-translation-id="sort_by_${o.name}" value="${o.name}" ${o.selected && 'selected'}>${o.label}</option>`
+      return `<option data-translation-id="sort_by_${_.snakeCase(o.name)}" value="${o.name}" ${o.selected && 'selected'}>${o.label}</option>`
     }).join('')
 
 
     const filters = this.statusOptions.map(s => {
-      return `<li class='filter-item'><input class="filter" type="checkbox" id="filter-${s.name}" value="${s.name}" checked><span class="legend--item--swatch" style="background-color: ${s.accessibleColor}"></span><label data-translation-id="filter_by_${s.name}" for="filter-${s.name}">${s.label}</label></li>`
+      return `<li class='filter-item'><input class="filter" type="checkbox" id="filter-${s.name}" value="${s.name}" checked><span class="legend--item--swatch" style="background-color: ${s.accessibleColor}"></span><label data-translation-id="filter_by_${_.snakeCase(s.name)}" for="filter-${s.name}">${s.label}</label></li>`
     }).join('')
 
     this.$controls.innerHTML = `
