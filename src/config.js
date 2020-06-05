395c43171c19cc88000ba8ddc55040c200f4494b
Omar White <omawhite@indiana.edu>
2020-06-05 15:15:33 -0500

    adds ensure function to config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,17 @@
 const { SNOWPACK_PUBLIC_MAPBOXGL_ACCESS_TOKEN, SNOWPACK_PUBLIC_DATA_URL, SNOWPACK_PUBLIC_TRANSLATION_URL} = import.meta.env
 
+/**
+ * Utilize this function to notify developer of required environment variables when developing
+ */
+function ensure(variableName, variable){
+  if(import.meta.env.MODE === 'development'){
+    console.error(`${variable_name} must be defined in your environment. See https://github.com/Twin-Cities-Mutual-Aid/twin-cities-aid-distribution-locations#environment-labels for details`)
+  }
+  variable
+}
 
 export default {
-  dataUrl: SNOWPACK_PUBLIC_DATA_URL,
-  accessToken: SNOWPACK_PUBLIC_MAPBOXGL_ACCESS_TOKEN,
-  translationUrl: SNOWPACK_PUBLIC_TRANSLATION_URL
+  dataUrl: ensure("SNOWPACK_PUBLIC_DATA_URL", SNOWPACK_PUBLIC_DATA_URL),
+  accessToken: ensure("SNOWPACK_PUBLIC_MAPBOXGL_ACCESS_TOKEN", SNOWPACK_PUBLIC_MAPBOXGL_ACCESS_TOKEN),
+  translationUrl: ensure("SNOWPACK_PUBLIC_TRANSLATION_URL", SNOWPACK_PUBLIC_TRANSLATION_URL),
 }
