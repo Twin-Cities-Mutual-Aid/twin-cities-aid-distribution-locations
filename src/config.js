682ee277e1c32b73b3fd32fe7099c489c5bb2854
Omar White <omawhite@indiana.edu>
2020-06-04 19:42:40 -0500

    fixes config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+const { MAPBOXGL_ACCESS_TOKEN, DATA_URL, TRANSLATION_URL} = import.meta
+
+
 export default {
-  dataUrl: 'https://spreadsheets.google.com/feeds/list/1CyPozeKhmOuIaVnKDQAUKKsvA9R4F20hEQ3MSGWczP8/1/public/full?alt=json',
-  accessToken: 'pk.eyJ1IjoiamFjb2JyZGFsdG9uIiwiYSI6ImNrYXc2anFjbjIxNGwyeG14dWk0MDVycmIifQ.yILWplracVMB4mv1dWwTtg',
-  translationUrl: 'https://spreadsheets.google.com/feeds/list/1m5QPNP6O8nsQsqJcQbwib_obpXRjmbYYGtwKzzg1l38/1/public/full?alt=json'
+  dataUrl: DATA_URL,
+  accessToken: MAPBOXGL_ACCESS_TOKEN,
+  translationUrl: TRANSLATION_URL
 }
