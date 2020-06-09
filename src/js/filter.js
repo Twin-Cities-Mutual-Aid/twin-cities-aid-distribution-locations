e1a7e199720b931ed1864b7e36be5eeac23ee706
Crhistian Ramirez <cramirez@four51.com>
2020-06-08 20:54:09 -0500

    Revert "hide clear search button instead of disable"
    This reverts commit f0dc10fe05fa8ab3fd9b784004cf3c455f11bd68.

--- src/js/filter.js
+++ src/js/filter.js
@@ -87,7 +87,6 @@ class Filter {
 
   search(event) {
     const searchTerm = event.target.value || ''
-    !searchTerm ? this.$clearSearchBtn.classList.add('hide-clear-search') : this.$clearSearchBtn.classList.remove('hide-clear-search');
     this.list.search(searchTerm, this.searchOptions.searchOn)
     const searchResults = this.list.items.filter(item => item.found).map(item => item.values().address);
     this.toggleMapPointsForSearch(searchTerm, searchResults)
@@ -122,7 +121,7 @@ class Filter {
           </div>
           <input type="text" class="search-input" id="search" placeholder="Search sites or needs..."></input>
         </div>
-        <button id="clear-search-btn" class="hide-clear-search">Clear Search</button>
+        <button id="clear-search-btn">Clear Search</button>
       </form>
     `
 
@@ -139,6 +138,8 @@ class Filter {
     this.$filters = Array.prototype.slice.call($key.querySelectorAll('input[type="checkbox"]'))
     this.$sort.addEventListener('change', this.update.bind(this))
     this.$search.addEventListener('input', event => {
+      const searchTerm = event.target.value || '';
+      this.$clearSearchBtn.disabled = !searchTerm;
       this.$locationList.classList.add('loading-indicator');
       debouncedSearch.bind(this)(event);
     })
@@ -148,9 +149,10 @@ class Filter {
     })
     this.$clearSearchBtn.addEventListener('click', event => {
       this.$search.value = '';
-      this.$clearSearchBtn.classList.add('hide-clear-search');
+      this.$clearSearchBtn.disabled = true;
       this.search.bind(this)(event)
     })
+    this.$clearSearchBtn.disabled = true;
     this.$filters.forEach($e => $e.addEventListener('change', this.update.bind(this)))
   }
 }
