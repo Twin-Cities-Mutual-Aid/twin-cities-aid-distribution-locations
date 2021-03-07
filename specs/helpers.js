6b1877398a93ea082dba9e5f75b919c4ef7c2ded
amaxama <maxam.anna@gmail.com>
2021-03-07 12:10:29 -0600

    Re-enable covid banner and fix test

--- specs/helpers.js
+++ specs/helpers.js
@@ -5,10 +5,9 @@ export const beforeEachTest = async t => {
     // Language must be selected before anything else can be done
     .click(Selector('.welcome-lang-button').withText('English'))
     // Hiatus modal must be cleared before tests
-      .click(Selector('.modal-close'))
+    // .click(Selector('.modal-close'))
     // Help window should not be visible at beginning
     .expect(Selector('#help-info').visible).notOk()
-  
   
 }
 
