279f245f2d1431d30b42670def313c0d08c197cc
amaxama <maxam.anna@gmail.com>
2021-03-07 12:23:30 -0600

    More thorough cleanup of modal

--- specs/helpers.js
+++ specs/helpers.js
@@ -4,8 +4,6 @@ export const beforeEachTest = async t => {
   await t
     // Language must be selected before anything else can be done
     .click(Selector('.welcome-lang-button').withText('English'))
-    // Hiatus modal must be cleared before tests
-    // .click(Selector('.modal-close'))
     // Help window should not be visible at beginning
     .expect(Selector('#help-info').visible).notOk()
   
